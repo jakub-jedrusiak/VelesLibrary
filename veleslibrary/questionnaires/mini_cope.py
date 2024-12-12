1a14a3fde81f0aefcadf4dc8a57aecba62e11f25
Jakub Jędrusiak <kuba23031999@gmail.com>
2024-12-12 18:27:52 +0100

    Correct typos

--- veleslibrary/questionnaires/mini_cope.py
+++ veleslibrary/questionnaires/mini_cope.py
@@ -1,6 +1,8 @@
 """Brief COPE (Mini-COPE)"""
+
 import velesresearch as vls
 from velesresearch.models import PageModel
+
 
 def mini_cope(
     name: str = "Mini_COPE",
@@ -11,18 +13,18 @@ def mini_cope(
     """
     ## Brief COPE (Mini-COPE)
         The questionnaire is used to assess ways of coping with stress. It is intended mainly for research purposes, but it can also be used in practice, in screening and preventive studies, and in assessing the effectiveness of therapeutic interventions. The Brief COPE omits two scales of the full COPE, reduces others to two items per scale, and adds one scale.
-    
+
     ## Original
         <div class="csl-bib-body" style="line-height: 2; margin-left: 2em; text-indent:-2em;">
-        <div class="csl-entry">Carver, C. S. (1997). You want to measure coping but your protocol’ too long: Consider the brief cope. <i>International Journal of Behavioral Medicine</i>, <i>4</i>(1), 92–100. <https://doi.org/10.1207/s15327558ijbm0401_6></div>
+        <div class="csl-entry">Carver, C. S. (1997). You want to measure coping but your protocol’s too long: Consider the brief cope. <i>International Journal of Behavioral Medicine</i>, <i>4</i>(1), 92–100. <https://doi.org/10.1207/s15327558ijbm0401_6></div>
         </div>
-    
+
     ## Score calculation
         An average.
 
     ## Reverse items
         None.
-    
+
     ## Subscales
         1. Active Coping: 1, 2
         2. Planning: 3, 4
@@ -76,7 +78,7 @@ def mini_cope(
     if pageOptions is None:
         pageOptions = {}
 
-    items = """I've been concentrating my efforts on doing something about the situation l'm in.
+    items = """I've been concentrating my efforts on doing something about the situation I'm in.
 I've been taking action to try to make the situation better.
 I've been trying to come up with a strategy about what to do.
 I've been thinking hard about what steps to take.
@@ -84,7 +86,7 @@ I've been trying to see it in a different light, to make it seem more positive.
 I've been looking for something good in what is happening.
 I've been accepting the reality of the fact that it has happened.
 I've been learning to live with it.
-l've been making jokes about it.
+I've been making jokes about it.
 I've been making fun of the situation.
 I've been trying to find comfort in my religion or spiritual beliefs.
 I've been praying or meditating.
@@ -96,8 +98,8 @@ I've been turning to work or other activities to take my mind off things.
 I've been doing something to think about it less, such as going to movies, watching TV, reading, daydreaming, sleeping, or shopping.
 I've been saying to myself "this isn't real."
 I've been refusing to believe that it has happened.
-l've been saying things to let my unpleasant feelings escape.
-i've been expressing my negative feelings.
+I've been saying things to let my unpleasant feelings escape.
+I've been expressing my negative feelings.
 I've been using alcohol or other drugs to make myself feel better.
 I've been using alcohol or other drugs to help me get through it.
 I've been giving up trying to deal with it.
@@ -107,11 +109,11 @@ I've been blaming myself for things that happened.""".split(
         "\n"
     )
 
-    scale = """0 – I haven't been doing this at all 
+    scale = """0 – I haven't been doing this at all
 1
 2
 3 – I've been doing this a lot""".split(
-  "\n"
+        "\n"
     )
 
     return vls.page(
